8ee344b78d1e62f2d29843b0da5b9d62141a8656
rquitales <quitales.ramon+github@gmail.com>
2018-08-01 13:16:50 -0700

    Kafka flush FIX.

--- src/streamData.py
+++ src/streamData.py
@@ -1,5 +1,6 @@
 import time, sys, random, json, csv
 from collections import defaultdict
+from datetime import datetime
 from kafka import KafkaProducer, KeyedProducer
 
 #Define Argument variables for readability.
@@ -9,13 +10,11 @@ seedFile = "../input/japanSafecast.txt"
 
 #Function to format new seed data entries into defaultdict structure.
 def insertEntry(dictionary, deviceID, latitude, longitude, value):
-    dictionary[str(deviceID)]["latitude"] = latitude
-    dictionary[str(deviceID)]["longitude"] = longitude
-    dictionary[str(deviceID)]["value"] = value
+    dictionary[str(deviceID)] = {'latitude': latitude, 'longitude' : longitude, 'value' : value, 'ctime' : str(datetime.now())}
 
 #Function to read unique number of lines in seed data file.
 def createSeedData(filename, numOfDevices = 100000):
-    seedData = defaultdict(dict)
+    seedData = {}
     with open(filename, 'r') as csvfile:
         d_id = 1
         for row in csv.DictReader(csvfile):
@@ -29,12 +28,14 @@ def createSeedData(filename, numOfDevices = 100000):
 def modifyReading(dictionary):
     for device in dictionary:
         dictionary[device]['value'] = dictionary[device]['value'] + random.randint(-5,5)
+        dictionary[device]['ctime'] = str(datetime.now())
 
 #Send data to kafka.
 def send2Kafka(ipAddresses, data, deviceKey):
     kProducer = (KafkaProducer(bootstrap_servers = ipAddresses,
               value_serializer = lambda v: json.dumps(v).encode('utf-8')))
-    kProducer.send(sensorTopic, {deviceKey: [data[deviceKey]['latitude'], data[deviceKey]['longitude'], data[deviceKey]['value']]})
+    kProducer.send(sensorTopic, {deviceKey: {'latitude' : data[deviceKey]['latitude'], 'longitude' : data[deviceKey]['longitude'], 'value' : data[deviceKey]['value'], 'ctime' : data[deviceKey]['ctime']}})
+    kProducer.flush()
 
 def main():
     #Create seed data.
